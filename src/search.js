500d57146b5aa78e938ba6dea7802ac7463c5b08
Mike Beck <mike@mikeybeck.com>
2024-02-12 16:21:58 +1300

    Display filter on search page

--- src/search.js
+++ src/search.js
@@ -7,6 +7,7 @@ chrome.runtime.onMessage.addListener((request) => {
         console.log(`Salary range (search page): ${request.result}`);
         getCachedJobs(async (cachedJobs) => {
             updateSearchPage(cachedJobs);
+            displayFilter();
         });
     }
 });
@@ -36,6 +37,80 @@ const updateSearchPage = (cachedJobs) => {
             showInfo(element, cachedJob);
         }
     }
+}
+
+const displayFilter = () => {
+    const numJobsElement = document.querySelectorAll("h1[data-automation=totalJobsMessage]");
+    if (numJobsElement.length === 0) {
+        console.log("No total jobs text found.  Unable to add filters.");
+        return;
+    }
+
+    // Get all tags from cached jobs
+    const tags = [];
+    getCachedJobs(async (cachedJobs) => {
+        for (const job of cachedJobs) {
+            console.log(`Job tags: ${job.tags}`);
+            const jobTags = JSON.parse(job.tags || '[]');
+            for (const tag of jobTags) {
+                tags.push(tag);
+            }
+        }
+
+        const uniqueTags = [...new Set(tags)];
+        console.log(`Unique tags: ${uniqueTags}`);
+        const filters = document.createElement("div");
+        filters.style.marginTop = "10px";
+        filters.style.marginBottom = "10px";
+        filters.style.display = "flex";
+        filters.style.flexWrap = "wrap";
+        filters.style.alignItems = "center";
+        filters.style.justifyContent = "flex-start";
+
+        const tagFilter = document.createElement("select");
+        tagFilter.id = "multipleSelect";
+        tagFilter.multiple = true;
+        // Add tags to filter
+        for (const tag of uniqueTags) {
+            const option = document.createElement("option");
+            option.value = tag;
+            option.text = tag;
+            tagFilter.appendChild(option);
+        }
+
+        // Add filter button
+        const filterButton = document.createElement("button");
+        filterButton.innerHTML = "Apply filter";
+        filterButton.onclick = applyFilter;
+        filterButton.style.marginLeft = "10px";
+        filterButton.style.padding = "10px";
+        filterButton.style.borderRadius = "5px";
+        filterButton.style.backgroundColor = "blue";
+        filterButton.style.color = "white";
+        filterButton.style.border = "none";
+        filterButton.style.cursor = "pointer";
+
+        filters.appendChild(tagFilter);
+        filters.appendChild(filterButton);
+
+        numJobsElement[0].appendChild(filters);
+    });
+}
+
+const applyFilter = () => {
+    var options = document.getElementById('multipleSelect').options,
+        result = [];
+
+    for (var i = 0, len = options.length; i < len; i++) {
+        var opt = options[i];
+
+        if (opt.selected) {
+            result.push(opt.value);
+        }
+    }
+
+    console.log(result);
+    alert(result);
 }
 
 const showInfo = async (element, cachedJob) => {
